62dffd39265236c91fff5466ddfa56def36f0705
Hazem Yahea <32298139+Hazemyahea@users.noreply.github.com>
2023-06-15 07:40:54 +0300

    Update codewars-Findarray-python.py

--- codewars-Findarray-python.py
+++ codewars-Findarray-python.py
@@ -1,10 +1,11 @@
+## Find array Qu from codeWars in py .. 
+## https://www.codewars.com/kata/59a2a3ba5eb5d4e609000055/train/python
 def find_array(arr1, arr2):
     arr3 = []
     if len(arr1) == 0 or len(arr2) == 0 :
         return arr3
     else:
         for x in arr2:
-            arr3.append(arr1[x])
+             if x < len(arr1):
+                arr3.append(arr1[x])
         return arr3
-
-print(find_array(["a","b","c","d"],[2,2,2]))
